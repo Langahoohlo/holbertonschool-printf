10545f32193f8386b77877e3c754c6dfa9d4b361
langahoohlo <langahoohlo@gmail.com>
2024-03-28 09:10:59 +0000

    printf

--- printf.c
+++ printf.c
@@ -6,7 +6,7 @@
  * print_char - print a character to stdout
  * @c: character to be pribted to stdout
  * @length: length of what is to be printed
-*/
+ */
 int print_char(char c, int length)
 {
     putchar(c);
@@ -18,8 +18,8 @@ int print_char(char c, int length)
  * @str: pointer to a string thats to be printed
  * @length: length of what is to be printed
  * Return: pointer
-*/
-int print_string(char *str, int length)
+ */
+int print_string(const char *str, int length)
 {
     int i;
     char nil[7] = "(null)";
@@ -29,7 +29,8 @@ int print_string(char *str, int length)
             putchar(str[i]);
         length += i;
         return (length);
-    } else
+    }
+    else
     {
         for (i = 0; nil[i]; i++)
             putchar(nil[i]);
@@ -41,64 +42,68 @@ int print_string(char *str, int length)
  * handle_mod - handle modulas sign
  * @length: length of what is to be printed
  * @n: used to determine whether to print two modulo signs or just one
-*/
-int handle_mod(int n, int length)
+ */
+int handle_mod(int length)
 {
-    if (n == 1)
-    {
-        putchar('%');
-        length++;
-        return (length);
-    }
-    else
-    {
-        putchar('%');
-        putchar('%');
-        length += 2;
-        return (length);
-    }
+    putchar('%');
+    length++;
+    return (length);
 }
 /**
  * _printf - print arguments
  * @format: pointer to character
  * Return: length of what is to be printed
  */
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
     int length = 0, i;
     va_list args;
     if (format == NULL)
         return (0);
     va_start(args, format);
-    for (i = 0; format[i]; i++) {
-        switch(format[i]) {
+    for (i = 0; format[i]; i++)
+    {
+        switch (format[i])
+        {
+        case '%':
+            switch (format[i + 1])
+            {
+            case 'c':
+                length = print_char(va_arg(args, int), length);
+                i++;
+                break;
+            case 's':
+                length = print_string(va_arg(args, char *), length);
+                i++;
+                break;
             case '%':
-                switch(format[i + 1]) {
-                    case 'c':
-                        length = print_char(va_arg(args, int), length);
-                        i++;
-                        break;
-                    case 's':
-                        length = print_string(va_arg(args, char *), length);
-                        i++;
-                        break;
-                    case '%':
-                        if (strlen(format) > 2)
-                        {
-                            length = handle_mod(1, length);
-                            i++;
-                        }
-                        else
-                            length = handle_mod(2, length);
+                if (strlen(format) > 2)
+                {
+                    length = handle_mod(length);
+                    i++;
                 }
                 break;
-                default:
-                        if (format[i] == '%')
-                            length--;
-                        else
-                            length = print_char(format[i], length);
-                        break;
+            case '\0':
+                length = print_char(format[i], length);
+                break;
+
+            default:
+                print_string(format, length);
+                i++;
+                break;
+            }
+            break;
+        default:
+            if (format[i] == '%')
+            {
+                length--;
+            }
+            else
+                length = print_char(format[i], length);
+            break;
         }
     }
     va_end(args);
+    
     return (length);
 }
